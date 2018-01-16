0be9bc4bbcdcca5809c5400316065eabc50cf1dc
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-01-16 14:04:55 +0200

    add factorial

--- README.md
+++ README.md
@@ -6,8 +6,16 @@
     * isSorted - Returns true or false, indicating whether the given array of numbers is sorted.
     * reverse - Reverses the given string (without reverse function).
     * myIndexOf - Implement the indexOf function for arrays.
-    * isPalindrome - Return true or false indicating whether the given string is a palindrome (case and space insensitive).
-    * missing - Takes an unsorted array of unique numbers (ie. no repeats) from 1 through some number n, and returns the missing number in the sequence (there are either no missing numbers, or exactly one missing number).
+    * isPalindrome - Return true or false indicating whether the given string is a palindrome 
+                    (case and space insensitive).
+    * missing - Takes an unsorted array of unique numbers (ie. no repeats) from 1 through some number n, 
+                and returns the missing number in the sequence (there are either no missing numbers, 
+                or exactly one missing number).
     * isBalanced - Takes a string and returns true or false indicating whether its curly braces are balanced.
     
-##### Please press F12 to see result      
+##### Please press F12 to see result  
+    
+### Link to: 
+    
+    * [a link](https://sergii5854.github.io/hw-js-12.2-function.github.io/) .
+
--- script.js
+++ script.js
@@ -1,125 +1,116 @@
 window.onload = function () {
-  
+
   function isPrime(num) {
-    for (var i = 2; i < num; i++) { // i will always be less than the parameter so the condition below will never allow parameter to be divisible by itself ex. (7 % 7 = 0) which would return true
-      if(num % i === 0) return false; // when parameter is divisible by i, it's not a prime number so return false
+    for (var i = 2; i < num; i++) {
+      if (num % i === 0) return false;
     }
     return num > 1;
   }
 
-
-
-  function factorial (num) {
-   
+  function factorial(num) {
+    return (num < 2) ? 1 : factorial(num - 1) * num;
   }
 
 
+  function fib(num) {
 
-  function fib (num) {
-  
-  }
-  
-
-
-
-  function isSorted (arr) {
-   
-  }
-  
-
-
-  function reverse (str) {
-  
-  }
-  
-
-
-  function indexOf (arr, num) {
-  
   }
 
 
-  function isPalindrome (str) {
-  
+  function isSorted(arr) {
+
   }
 
 
- function missing(array) {
-   
+  function reverse(str) {
+
   }
 
-  function isBalanced (str) {
-   
+
+  function indexOf(arr, num) {
+
+  }
+
+
+  function isPalindrome(str) {
+
+  }
+
+
+  function missing(array) {
+
+  }
+
+  function isBalanced(str) {
+
   }
 
   console.group('isPrime');
   console.info(isPrime(0), 'right answer is : false ');
-  console.info(isPrime(1) , 'right answer is : false ');
-  console.info(isPrime(17)  , 'right answer is : true ');
+  console.info(isPrime(1), 'right answer is : false ');
+  console.info(isPrime(17), 'right answer is : true ');
   console.info(isPrime(10000000000000), 'right answer is : false ');
   console.groupEnd();
 
 
   console.group('Factorial');
-  console.info(factorial(0) , 'right answer is : 1 ');
-  console.info(factorial(1) , 'right answer is : 1 ');
-  console.info( factorial(6) , 'right answer is : 720 ');
+  console.info(factorial(0), 'right answer is : 1 ');
+  console.info(factorial(1), 'right answer is : 1 ');
+  console.info(factorial(6), 'right answer is : 720 ');
   console.groupEnd();
 
 
   console.group('Fibonacci');
-  console.info(fib(0), 'right answer is : 0 ');                         
-  console.info(fib(1) , 'right answer is : 1 ');                           
-  console.info(fib(10) , 'right answer is : 55 ');
-  console.info(fib(20) , 'right answer is : 6765 ');
+  console.info(fib(0), 'right answer is : 0 ');
+  console.info(fib(1), 'right answer is : 1 ');
+  console.info(fib(10), 'right answer is : 55 ');
+  console.info(fib(20), 'right answer is : 6765 ');
   console.groupEnd();
-  
+
 
   console.group('isSorted');
-  console.info( isSorted([]) , 'right answer is : true ');                        
-  console.info( isSorted([-Infinity, -5, 0, 3, 9]) , 'right answer is : true ');   
-  console.info( isSorted([3, 9, -3, 10])  , 'right answer is : false ');           
+  console.info(isSorted([]), 'right answer is : true ');
+  console.info(isSorted([-Infinity, -5, 0, 3, 9]), 'right answer is : true ');
+  console.info(isSorted([3, 9, -3, 10]), 'right answer is : false ');
   console.groupEnd();
-  
-  
+
+
   console.group('reverse');
-  console.info( reverse('')   , 'right answer is : "" ');
-  console.info( reverse('abcdef')   , 'right answer is : fedcba ');
+  console.info(reverse(''), 'right answer is : "" ');
+  console.info(reverse('abcdef'), 'right answer is : fedcba ');
   console.groupEnd();
-  
+
   console.group('indexOf');
-  console.info(  indexOf([1, 2, 3], 1)  , 'right answer is : 0 ');                 
-  console.info( indexOf([1, 2, 3], 4)   , 'right answer is : -1 ');                 
+  console.info(indexOf([1, 2, 3], 1), 'right answer is : 0 ');
+  console.info(indexOf([1, 2, 3], 4), 'right answer is : -1 ');
   console.groupEnd();
-  
-  
+
+
   console.group('isPalindrome');
-  console.info( isPalindrome('')    , 'right answer is : true ');                                
-  console.info(isPalindrome('abcdcba') , 'right answer is : true ');                        
-  console.info( isPalindrome('abcd')   , 'right answer is : false ');                          
-  console.info( isPalindrome('A man a plan a canal Panama'), 'right answer is : true ');
+  console.info(isPalindrome(''), 'right answer is : true ');
+  console.info(isPalindrome('abcdcba'), 'right answer is : true ');
+  console.info(isPalindrome('abcd'), 'right answer is : false ');
+  console.info(isPalindrome('A man a plan a canal Panama'), 'right answer is : true ');
   console.groupEnd();
 
 
   console.group('missing');
-  console.info( missing([])   , 'right answer is : undefined ');                       
-  console.info( missing([1, 4, 3])   , 'right answer is : 2 ');               
-  console.info( missing([2, 3, 4])   , 'right answer is : 1 ');                
-  console.info( missing([5, 1, 4, 2])   , 'right answer is : 3 ');           
-  console.info( missing([1, 2, 3, 4])  , 'right answer is : undefined ');              
+  console.info(missing([]), 'right answer is : undefined ');
+  console.info(missing([1, 4, 3]), 'right answer is : 2 ');
+  console.info(missing([2, 3, 4]), 'right answer is : 1 ');
+  console.info(missing([5, 1, 4, 2]), 'right answer is : 3 ');
+  console.info(missing([1, 2, 3, 4]), 'right answer is : undefined ');
   console.groupEnd();
 
 
   console.group('isBalanced');
-  console.info(isBalanced('}{')   , 'right answer is : false ');                   
-  console.info(  isBalanced('{{}')   , 'right answer is : false ');                   
-  console.info( isBalanced('{}{}')    , 'right answer is : false ');                
-  console.info(  isBalanced('foo { bar { baz } boo }'), 'right answer is : true ');  
-  console.info( isBalanced('foo { bar { baz }') , 'right answer is : false ');       
-  console.info(  isBalanced('foo { bar } }')    , 'right answer is : false ');        
+  console.info(isBalanced('}{'), 'right answer is : false ');
+  console.info(isBalanced('{{}'), 'right answer is : false ');
+  console.info(isBalanced('{}{}'), 'right answer is : false ');
+  console.info(isBalanced('foo { bar { baz } boo }'), 'right answer is : true ');
+  console.info(isBalanced('foo { bar { baz }'), 'right answer is : false ');
+  console.info(isBalanced('foo { bar } }'), 'right answer is : false ');
   console.groupEnd();
-  
-  
-  
-  
+
+
 };
